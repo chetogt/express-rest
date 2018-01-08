a832f68928a26c84a9129569e8ca5435d7a2dc52
Jose Cordova <jacp@jumay.biz>
2018-01-07 21:26:08 -0600

    Se agrega body-parser Se agrega metodo POST Reestructuracion de codigo

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/libraryApp', {
     useMongoClient: true
@@ -11,36 +12,13 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-bookRouter.route('/books')
-    .get(function (req, res) {
-        var query = {};
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
+bookRouter = require('./routes/bookRoutes')(Book);
 
-        Book.find(query, function (err, books) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(books);          
-            }
-        });
-    });
-
-bookRouter.route('/books/:bookId')
-.get(function (req, res) {
-    Book.findById(req.params.bookId, function (err, book) {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.json(book);          
-        }
-    });
-});
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter);
 
 app.get('/', function (req, res) {
     res.send('Welcome to my API');
